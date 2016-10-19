a5c3fbd31e1ebd05cd9687e5bf67ff7c9b98f22f
Gustav Hedengran <gustav.hedengran@gmail.com>
2016-10-19 11:30:53 +0200

    added vundlereinstall to vimsetup script

--- vimSetup.sh
+++ vimSetup.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/hedengran/vim-configs.git
-mv vim-configs .vim
-bash ~/.vim/vundleReinstall.sh
+git clone https://github.com/hedengran/vim-configs.git ~/.vim
+git rm -rf ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ~/.vim/.vimrc ~/.vimrc
 vim +PluginInstall +qall

015607c2cb9ee244270138ee07efaee679590691
hedengran <gustav.hedengran@gmail.com>
2016-11-18 15:48:38 +0100

    Update bashSetup.sh

--- bashSetup.sh
+++ bashSetup.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/hedengran/bash-configs.git ~/bash
+git clone https://github.com/hedengran/bash_configs.git ~/bash
 cp ~/bash/.bash_profile ~/.bash_profile
 cp ~/bash/bash_profile ~/.bashrc
 source ~/.bash_profile

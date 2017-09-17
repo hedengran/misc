f92856a7dec013ea5150de6c77d2faf18eb3b420
hedengran <gustav.hedengran@gmail.com>
2017-09-17 14:30:44 +0200

    documentation to dotfilesScript.sh

--- dotfilesSetup.sh
+++ dotfilesSetup.sh
@@ -1,6 +1,8 @@
 git clone --bare https://github.com/hedengran/dotfiles.git $HOME/.myconf
 alias config='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME'
 
+#finds previous dotfiles and stores in backup folder
+#does not work for folders
 mkdir -p .config-backup && \
 config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
 xargs -I{} mv {} .config-backup/{}

4aeba0f570b2e8d769227c84044167da37e6f772
Gustav Hedengran <gustav.hedengran@gmail.com>
2017-09-17 14:16:23 +0200

    updated script to handle vim packagemanager

--- dotfilesSetup.sh
+++ dotfilesSetup.sh
@@ -1,4 +1,6 @@
-git clone --bare https://github.com/hedengran/dotfiles.git
+git clone --bare https://github.com/hedengran/dotfiles.git $HOME/.myconf
 alias config='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME'
 config checkout
 config config --local status.showUntrackedFiles no
+
+git clone https:///github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim

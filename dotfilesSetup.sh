c243d502d6d25c2bf831fee1305802de958232c9
hedengran <gustav.hedengran@gmail.com>
2017-09-17 14:22:34 +0200

    Update dotfilesSetup.sh

--- dotfilesSetup.sh
+++ dotfilesSetup.sh
@@ -1,5 +1,10 @@
 git clone --bare https://github.com/hedengran/dotfiles.git $HOME/.myconf
 alias config='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME'
+
+mkdir -p .config-backup && \
+config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .config-backup/{}
+
 config checkout
 config config --local status.showUntrackedFiles no
 
